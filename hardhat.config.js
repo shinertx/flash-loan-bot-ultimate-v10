f721578b490e1b9708263873cbfa755e2299a6f2
shinertx <168613225+shinertx@users.noreply.github.com>
2025-02-09 19:45:26 -0600

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,22 @@
+require('dotenv').config();
+require('@nomicfoundation/hardhat-toolbox');
+
+const { RPC_URL, WALLET_PRIVATE_KEY } = process.env;
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: RPC_URL,
+      accounts: [WALLET_PRIVATE_KEY]
+    },
+    hardhat: {
+      forking: {
+        url: RPC_URL,
+        blockNumber: 17400000
+      },
+      allowUnlimitedContractSize: true
+    }
+  }
+};
 
