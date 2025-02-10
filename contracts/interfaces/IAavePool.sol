08f8318f424f70a93caf192c0b06c3267c55b32c
shinertx <168613225+shinertx@users.noreply.github.com>
2025-02-09 19:58:56 -0600

    Create IAavePool.sol

---
+++ contracts/interfaces/IAavePool.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.19;
+
+interface IAavePool {
+    function flashLoanSimple(
+        address receiverAddress,
+        address asset,
+        uint256 amount,
+        bytes calldata params,
+        uint16 referralCode
+    ) external;
+}
