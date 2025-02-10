7d6843d5e9fb8f2a5a552598eefd897d74f8eae0
shinertx <168613225+shinertx@users.noreply.github.com>
2025-02-09 19:59:31 -0600

    Update IERC20.sol

--- contracts/interfaces/IERC20.sol
+++ contracts/interfaces/IERC20.sol
@@ -1,1 +1,8 @@
+pragma solidity ^0.8.19;
+
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+}
 
