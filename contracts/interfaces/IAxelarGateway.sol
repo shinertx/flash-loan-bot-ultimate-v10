6e6f701eba7875a274878461d6343a82dd20a0f3
shinertx <168613225+shinertx@users.noreply.github.com>
2025-02-09 19:59:48 -0600

    Update IAxelarGateway.sol

--- contracts/interfaces/IAxelarGateway.sol
+++ contracts/interfaces/IAxelarGateway.sol
@@ -1,1 +1,6 @@
+pragma solidity ^0.8.19;
+
+interface IAxelarGateway {
+    function callContract(string calldata destinationChain, string calldata contractAddress, bytes calldata payload) external;
+}
 
