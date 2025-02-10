b8ae9ce9de31707497475bcfe3847fc2cac76e88
shinertx <168613225+shinertx@users.noreply.github.com>
2025-02-09 19:48:25 -0600

    Update IFlashLoanSimpleReceiver.sol

--- contracts/interfaces/IFlashLoanSimpleReceiver.sol
+++ contracts/interfaces/IFlashLoanSimpleReceiver.sol
@@ -1,1 +1,12 @@
+pragma solidity ^0.8.19;
+
+interface IFlashLoanSimpleReceiver {
+    function executeOperation(
+        address asset,
+        uint256 amount,
+        uint256 premium,
+        address initiator,
+        bytes calldata params
+    ) external returns (bool);
+}
 
