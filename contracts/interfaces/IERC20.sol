d76ccc7ca1f4c16cf24d9c59a43c5d34f83c976b
shinertx <168613225+shinertx@users.noreply.github.com>
2025-03-09 05:07:32 -0500

    Update IERC20.sol

--- contracts/interfaces/IERC20.sol
+++ contracts/interfaces/IERC20.sol
@@ -5,4 +5,3 @@ interface IERC20 {
     function approve(address spender, uint256 amount) external returns (bool);
     function transfer(address recipient, uint256 amount) external returns (bool);
 }
-
