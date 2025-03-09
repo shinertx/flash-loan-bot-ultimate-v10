b4190a74e6cf7ca143eb144719d13acb1f1c3d26
shinertx <168613225+shinertx@users.noreply.github.com>
2025-03-09 05:16:16 -0500

    Update arbitrage_utils.js

--- utils/arbitrage_utils.js
+++ utils/arbitrage_utils.js
@@ -1,1 +1,18 @@
+function calculateUniswapV2Output(amountIn, reserveIn, reserveOut) {
+    const amountInWithFee = BigInt(amountIn) * 997n;
+    const numerator = amountInWithFee * BigInt(reserveOut);
+    const denominator = BigInt(reserveIn) * 1000n + amountInWithFee;
+    return numerator / denominator;
+}
+
+function calculateUniswapV2Input(amountOut, reserveIn, reserveOut) {
+    const numerator = BigInt(reserveIn) * BigInt(amountOut) * 1000n;
+    const denominator = (BigInt(reserveOut) - BigInt(amountOut)) * 997n;
+    return (numerator / denominator) + 1n;
+}
+
+module.exports = {
+    calculateUniswapV2Output,
+    calculateUniswapV2Input
+};
 
