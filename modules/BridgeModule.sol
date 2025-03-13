210aaa9d63d9cde1b82a7caf4c5c4225104a25b0
shinertx <168613225+shinertx@users.noreply.github.com>
2025-03-13 11:11:33 -0500

    Update BridgeModule.sol

--- modules/BridgeModule.sol
+++ modules/BridgeModule.sol
@@ -1,40 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "../contracts/interfaces/IERC20.sol";
-import "../contracts/interfaces/IAxelarGateway.sol";
+import "../interfaces/IERC20.sol";
+import "../interfaces/IAxelarGateway.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract BridgeModule is Ownable {
-  using SafeERC20 for IERC20;
+    using SafeERC20 for IERC20;
 
-  address public axelarGateway;
-  address public dai;
+    address public axelarGateway;
+    address public dai;
 
-  // Adjust these to your needs:
-  string public destinationChain = "Moonbeam"; 
-  string public destinationAddress = "0xRecipientOnDestChain"; 
+    string public destinationChain = "Moonbeam"; 
+    string public destinationAddress = "0xRecipientOnDestChain";
 
-  constructor(address _axelarGateway, address _dai) {
-    axelarGateway = _axelarGateway;
-    dai = _dai;
-  }
+    constructor(address _axelarGateway, address _dai) {
+        axelarGateway = _axelarGateway;
+        dai = _dai;
+    }
 
-  function setDestinationChain(string calldata chain, string calldata contractAddr) external onlyOwner {
-    destinationChain = chain;
-    destinationAddress = contractAddr;
-  }
+    function setDestinationChain(string calldata chain, string calldata contractAddr) external onlyOwner {
+        destinationChain = chain;
+        destinationAddress = contractAddr;
+    }
 
-  function bridgeTokens(address token, uint256 amount) external onlyOwner {
-    // Example bridging: Approve Axelar, then call
-    IERC20(token).safeApprove(axelarGateway, 0);
-    IERC20(token).safeApprove(axelarGateway, amount);
+    function bridgeTokens(address token, uint256 amount) external onlyOwner {
+        IERC20(token).safeApprove(axelarGateway, 0);
+        IERC20(token).safeApprove(axelarGateway, amount);
 
-    IAxelarGateway(axelarGateway).callContract(
-      destinationChain,
-      destinationAddress,
-      abi.encode(token, amount)
-    );
-  }
+        IAxelarGateway(axelarGateway).callContract(
+            destinationChain,
+            destinationAddress,
+            abi.encode(token, amount)
+        );
+    }
 }
